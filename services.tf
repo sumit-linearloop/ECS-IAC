69a68b833f4f122a3468137fcdedb48f909f3982
sumit-linearloop <sumit@linearloop.io>
2024-09-09 14:40:31 +0530

    Create services.tf

---
+++ services.tf
@@ -0,0 +1,31 @@
+# ECS Services
+resource "aws_ecs_service" "app" {
+  name            = "app"
+  cluster         = aws_ecs_cluster.my_vpc.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+
+    load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app"  # Must match the container name in your task definition
+    container_port   = 1400             # Must match the container's port exposed in the task definition
+  }
+
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.my_vpc.name
+    base              = 1
+    weight            = 100
+  }
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
+
+
