4df5237a1a3a6176958f4c8de329e0d64a8b52c3
sumit-linearloop <sumit@linearloop.io>
2024-09-10 15:03:33 +0530

    123

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
-provider "aws" {
-  region     = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "ap-south-1" # Correctly specify the region
+}
+
